5b8f4ff19a9eeaa93efa1859ca8722d2f1ddb7a7
Avetisyaann <Avetisyaann>
2026-05-19 18:44:06 +0400

    wow

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const cont = document.getElementById('event')
 
 cont.addEventListener('click', event => {
     let evTarget = event.target.innerText
-    if(evTarget === '='){
+    if (evTarget === '=') {
         display.innerText = eval(display.innerText)
-    }else{
+    } else if (evTarget === 'C') {
+        display.innerText = ''
+    } else if (evTarget === 'AC') {
+        display.innerText = display.innerText.substring(0,display.innerText.length-1)
+    }else if(evTarget === 'sqrt'){
+        display.innerText = Math.sqrt(display.innerText)
+    }else if(evTarget === 'pow'){
+        display.innerText = Math.pow(display.innerText,2)
+    }
+    else {
         display.innerText = display.innerText + evTarget
     }
 })
